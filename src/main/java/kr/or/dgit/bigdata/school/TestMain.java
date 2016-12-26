51995ca393b90e9d4f808c74bb5f1f3db3f5ce32
kyunga <dnldzmdnldzm@naver.com>
2016-12-26 16:14:23 +0900

    TEST완료

---
+++ src/main/java/kr/or/dgit/bigdata/school/TestMain.java
@@ -0,0 +1,12 @@
+package kr.or.dgit.bigdata.school;
+
+import javax.swing.JFrame;
+
+public class TestMain {
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setTitle("테스트");
+		jf.setVisible(true);
+		jf.setBounds(100, 100, 300, 200);
+	}
+}
